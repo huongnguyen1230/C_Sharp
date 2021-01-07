2cf02c1cfb3b6d5373f7b0b8e1d77d813b7be4cd
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-01-07 22:49:45 +0700

    LAB5

---
+++ C#/LAB5/ASM01/IMT/MedicinesAvailable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMT
+{
+    class MedicinesAvailable
+    {
+        int TotalNumberOfMedicines;
+        string MedicineName;
+        DateTime ExpiryDate;
+    }
+
+    public void AcceptMedicine()
+    {
+        Console.Write("Enter ");
+    }
+}
