340aa0071d48388fbeab83dcc68b4cdb258a4583
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-01-20 21:34:46 +0700

    LAB8

---
+++ C#/LAB8/ArrayList/Ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Ex1
+{
+    class ArrayCollection
+    {
+        static void Main(string[] args)
+        {
+            ArrayList objList = new ArrayList();
+            objList.Add("John");
+            objList.Add("James");
+            objList.Add("Peter");
+            objList.RemoveAt(2);
+            objList.Insert(2, "Williams");
+
+            Console.WriteLine("Capacity: " + objList.Capacity);
+            Console.WriteLine("Count: " + objList.Count);
+            Console.WriteLine();
+            Console.WriteLine("Elements of the ArrayList");
+            foreach(string str in objList)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ C#/LAB8/ArrayList/Ex3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex3
+{
+    static class ExtensionExample
+    {
+        public static List<T> RemoveDuplicate<T>(this List<T> allCities)
+        {
+            List<T> finalCities = new List<T>();
+            foreach (var eachCity in allCities)
+                if (!finalCities.Contains(eachCity))
+                    finalCities.Add(eachCity);
+            return finalCities;
+        }
+    }
+    class Program
+    {
+     static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+            cities.Add("Seoul");
+            cities.Add("Berlin ");
+            cities.Add("Istanbul");
+            cities.Add("Istanbul");
+            cities.Add("Paris");
+
+            List<string> result = cities.RemoveDuplicate();
+            foreach (string city in result)
+                Console.WriteLine(city);
+        }
+     }
+}
